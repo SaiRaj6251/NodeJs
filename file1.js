ae42bc0b8c236eac376940942af206dc506242ab
SaiRaj6251 <sairajpawar80@gmail.com>
2023-09-25 19:27:30 +0530

    sync-async done

---
+++ file1.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require("fs");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const second = result;
+    writeFile(
+      `./content/result-async.txt`,
+      `Here is the result ,${first} and ,${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+
+});
+// async is fast that sync as sync goes line by line and also as async has too many messed up callbacks so use promises and awake/await
