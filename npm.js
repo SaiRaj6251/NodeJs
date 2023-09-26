45fe92a55a3107c5deac8506ff7caa22be829dc2
SaiRaj6251 <sairajpawar80@gmail.com>
2023-09-26 18:16:43 +0530

    nodemon done

--- npm.js
+++ npm.js
@@ -10,3 +10,16 @@ const _ = require('lodash');
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
 console.log(newItems)
+
+/* generally while sharing code with another developer on github we dont share node_modules but as package json is 
+   shared so using ....npm install....he can install all the node modules */
+
+/* nodemon module is used to do always start of project and run it and we are installing it as dev dependency as
+    it can also be installed as simple dependency by :
+    npm i nodemon (-D or --save-dev) 
+    then in package.json : "scripts" : { "start" : "node app.js"
+                                            "dev" : "nodemon app.js" } and in cmd run : npm run dev(cmd_name)
+                                            or the other way ,
+                                            {"scripts" : "nodemon app.js"} and in cmd : npm start
+    
+    */
